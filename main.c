4b18eb9917d9a1f676779cf1d05aef65cbc6108d
João Marinho <joao.marinho@outlook.com>
2016-11-03 20:58:38 -0200

    macros created

--- main.c
+++ main.c
@@ -5,50 +5,38 @@
 #include "lexer.h"
 #include "automata.h"
 
-int keyword_types_check(Token* token) {
-  if(token->type == Keyword) {
-    token_print(token);
-    return 1;
-  }
-
-  return 0;
+#define FOO(name) AUTOMATA_KEYWORK(name)
+#define AUTOMATA_KEYWORK(name) int keyword_##name ## _check(Token* token) { \
+  if(token->type == Keyword) { \
+    token_print(token); \
+    return 1; \
+  } \
+\
+  return 0; \
 }
 
-int keyword_as_check(Token* token) {
-  if(token->type == Keyword) {
-    token_print(token);
-    return 1;
-  }
-
-  return 0;
+#define AUTOMATA_DELIMITER(name) int delimiter_##name ## _check(Token* token) { \
+  if(token->type == Delimiter) { \
+    token_print(token); \
+    return 1; \
+  } \
+\
+  return 0; \
 }
 
-int delimiter_open_braces_check(Token* token) {
-  if(token->type == Delimiter) {
-    token_print(token);
-    return 1;
-  }
 
-  return 0;
-}
+#define AUTOMATA_TRANSITION_INIT(name) Automata* name ## _automata = automata_transition_init(&name, #name)
 
-int delimiter_close_braces_check(Token* token) {
-  if(token->type == Delimiter) {
-    token_print(token);
-    return 1;
-  }
+AUTOMATA_KEYWORK(types);
+AUTOMATA_KEYWORK(char);
+AUTOMATA_KEYWORK(bool);
+AUTOMATA_KEYWORK(float);
+AUTOMATA_KEYWORK(int);
+AUTOMATA_KEYWORK(as);
 
-  return 0;
-}
-
-int delimiter_semicolon_check(Token* token) {
-  if(token->type == Delimiter) {
-    token_print(token);
-    return 1;
-  }
-
-  return 0;
-}
+AUTOMATA_DELIMITER(open_braces);
+AUTOMATA_DELIMITER(close_braces);
+AUTOMATA_DELIMITER(semicolon);
 
 Automata* create_program_automata() {
   Automata* program_automata = automata_init(5, "program"); //OK
@@ -60,13 +48,27 @@ Automata* create_program_automata() {
   Automata* renomear_automata = automata_init(5, "renomear"); //OK
   Automata* estrutura_automata = automata_init(2, "estrutura");
   Automata* nome_automata = automata_init(2, "nome");
-  Automata* tipo_base_automata = automata_init(2, "tipo_base");
+  Automata* tipo_base_automata = automata_init(2, "tipo_base"); //OK
 
-  Automata* keyword_types_check_automata = automata_transition_init(&keyword_types_check, "keyword_types_check");
-  Automata* keyword_as_check_automata = automata_transition_init(&keyword_as_check, "keyword_as_check");
-  Automata* delimiter_open_braces_check_automata = automata_transition_init(&delimiter_open_braces_check, "delimiter_open_braces_check");
-  Automata* delimiter_close_braces_check_automata = automata_transition_init(&delimiter_close_braces_check, "delimiter_close_braces_check");
-  Automata* delimiter_semicolon_check_automata = automata_transition_init(&delimiter_semicolon_check, "delimiter_semicolon_check");
+  // Automata* keyword_types_check_automata = automata_transition_init(&keyword_types_check, "keyword_types_check");
+  // Automata* keyword_as_check_automata = automata_transition_init(&keyword_as_check, "keyword_as_check");
+  // Automata* keyword_char_check_automata = automata_transition_init(&keyword_char_check, "keyword_char_check");
+  // Automata* keyword_bool_check_automata = automata_transition_init(&keyword_bool_check, "keyword_bool_check");
+  // Automata* keyword_float_check_automata = automata_transition_init(&keyword_float_check, "keyword_float_check");
+  // Automata* keyword_int_check_automata = automata_transition_init(&keyword_int_check, "keyword_int_check");
+  // Automata* delimiter_open_braces_check_automata = automata_transition_init(&delimiter_open_braces_check, "delimiter_open_braces_check");
+  // Automata* delimiter_close_braces_check_automata = automata_transition_init(&delimiter_close_braces_check, "delimiter_close_braces_check");
+  // Automata* delimiter_semicolon_check_automata = automata_transition_init(&delimiter_semicolon_check, "delimiter_semicolon_check");
+
+  AUTOMATA_TRANSITION_INIT(keyword_types_check);
+  AUTOMATA_TRANSITION_INIT(keyword_as_check);
+  AUTOMATA_TRANSITION_INIT(keyword_char_check);
+  AUTOMATA_TRANSITION_INIT(keyword_bool_check);
+  AUTOMATA_TRANSITION_INIT(keyword_float_check);
+  AUTOMATA_TRANSITION_INIT(keyword_int_check);
+  AUTOMATA_TRANSITION_INIT(delimiter_open_braces_check);
+  AUTOMATA_TRANSITION_INIT(delimiter_close_braces_check);
+  AUTOMATA_TRANSITION_INIT(delimiter_semicolon_check);
 
   //PROGRAM AUTOMATA
   automata_set_final_state(program_automata, 1);
@@ -99,11 +101,18 @@ Automata* create_program_automata() {
   automata_add_transition(tipo_automata, 0, 1, renomear_automata);
 
   //RENOMEAR AUTOMATA
-  automata_set_final_state(renomear_automata, 1);
+  automata_set_final_state(renomear_automata, 4);
   automata_add_transition(renomear_automata, 3, 4, delimiter_semicolon_check_automata);
   automata_add_transition(renomear_automata, 2, 3, nome_automata);
   automata_add_transition(renomear_automata, 1, 2, keyword_as_check_automata);
   automata_add_transition(renomear_automata, 0, 1, tipo_base_automata);
+
+  //TIPO_BASE AUTOMATA
+  automata_set_final_state(tipo_base_automata, 1);
+  automata_add_transition(tipo_base_automata, 0, 1, keyword_char_check_automata);
+  automata_add_transition(tipo_base_automata, 0, 1, keyword_bool_check_automata);
+  automata_add_transition(tipo_base_automata, 0, 1, keyword_float_check_automata);
+  automata_add_transition(tipo_base_automata, 0, 1, keyword_int_check_automata);
 
   return program_automata;
 }
